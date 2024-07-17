b777082034cde43ae57eee18905e0f434bf73268
Alex Petrenko <alex.pt@ya.ru>
2024-07-17 18:42:22 +0300

    delete storyboard

---
+++ RickAndMorty/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  RickAndMorty
+//
+//  Created by Алексей on 17.07.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: HomeScreen())
+        window?.makeKeyAndVisible()
+    }
+}
+
