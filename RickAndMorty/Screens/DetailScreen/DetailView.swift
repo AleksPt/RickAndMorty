26921816fa19463267055a7f73cb00489558f673
Alex Petrenko <alex.pt@ya.ru>
2024-07-18 12:34:43 +0300

    add NSAttributedString

--- RickAndMorty/Screens/DetailScreen/DetailView.swift
+++ RickAndMorty/Screens/DetailScreen/DetailView.swift
@@ -25,7 +25,7 @@ final class DetailView: UIView {
         return element
     }()
     
-    private lazy var imageView: UIImageView = {
+    lazy var imageView: UIImageView = {
         let element = UIImageView()
         element.layer.cornerRadius = 12
         element.clipsToBounds = true
@@ -34,7 +34,7 @@ final class DetailView: UIView {
         return element
     }()
     
-    private lazy var statusView: UIView = {
+    lazy var statusView: UIView = {
         let element = UIView()
         element.translatesAutoresizingMaskIntoConstraints = false
         element.layer.cornerRadius = 16
@@ -45,13 +45,13 @@ final class DetailView: UIView {
     
     private let statusLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.semiboldLabel)
     
-    private let speciesLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.semiboldLabel, numberOflines: 0)
+    private let speciesLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.regularLabel, numberOflines: 0)
     
-    private let genderLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.semiboldLabel, numberOflines: 0)
+    private let genderLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.regularLabel, numberOflines: 0)
     
-    private let episodesLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.semiboldLabel, numberOflines: 0)
+    private let episodesLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.regularLabel, numberOflines: 0)
     
-    private let locationLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.semiboldLabel, numberOflines: 0)
+    private let locationLabel = LabelFactory.makeLabel(font: UIFont.TextFonts.DetailScreen.regularLabel, numberOflines: 0)
     
     private lazy var vStack: UIStackView = {
         let element = UIStackView()
@@ -76,13 +76,6 @@ final class DetailView: UIView {
         backgroundColor = .black
         addSubview(background)
         setupConstraints()
-        
-        imageView.image = .mockImg6
-        statusLabel.text = "Alive"
-        speciesLabel.text = "Species: Human"
-        genderLabel.text = "Gender: Male"
-        episodesLabel.text = "Episodes: 6, 11, 30"
-        locationLabel.text = "Last known location: Earth"
     }
     
     required init?(coder: NSCoder) {
@@ -93,6 +86,43 @@ final class DetailView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         imageView.heightAnchor.constraint(equalToConstant: background.frame.width - 32).isActive = true
+    }
+    
+    // MARK: - Public methods
+    func setupView(
+        image: UIImage,
+        status: String,
+        species: String,
+        gender: String,
+        episodes: String,
+        location: String
+    ) {
+        imageView.image = image
+        statusLabel.text = status
+        speciesLabel.attributedText = LabelFactory.makeBoldAttributedString(
+            fullString: "Species: " + species,
+            highlightedSubstring: "Species:"
+        )
+        genderLabel.attributedText = LabelFactory.makeBoldAttributedString(
+            fullString: "Gender: " + gender,
+            highlightedSubstring: "Gender:"
+        )
+        episodesLabel.attributedText = LabelFactory.makeBoldAttributedString(
+            fullString: "Episodes: " + episodes,
+            highlightedSubstring: "Episodes:"
+        )
+        locationLabel.attributedText = LabelFactory.makeBoldAttributedString(
+            fullString: "Last known location: " + location,
+            highlightedSubstring: "Last known location:"
+        )
+        
+        if status.lowercased().contains("alive") {
+            statusView.backgroundColor = UIColor.surfGreen
+        } else if status.lowercased().contains("dead") {
+            statusView.backgroundColor = UIColor.surfRed
+        } else {
+            statusView.backgroundColor = UIColor.surfGray
+        }
     }
 }
 
@@ -107,7 +137,7 @@ private extension DetailView {
             statusView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 12),
             statusView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
             statusView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
-            statusView.heightAnchor.constraint(equalToConstant: 31.92),
+            statusView.heightAnchor.constraint(equalToConstant: 41.92),
             
             statusLabel.centerYAnchor.constraint(equalTo: statusView.centerYAnchor),
             statusLabel.centerXAnchor.constraint(equalTo: statusView.centerXAnchor),
